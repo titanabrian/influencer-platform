c0ea2581c7adb2c333c68909250cf50a9159654c
titanabrianwork <titanabrianwork@gmail.com>
2020-02-18 05:30:54 +0700

    Backend api middleware

---
+++ middleware/authmiddleware.js
@@ -0,0 +1,25 @@
+let axios = require("axios");
+var result;
+
+module.exports=async function(req,res,next){    
+
+    if(typeof(req.headers.authorization)=="undefined"){
+        res.status(401)
+        return res.json({msg:"Unauthorized request"});
+    }else{
+        let options={
+            method:"POST",
+            url:"https://www.googleapis.com/oauth2/v3/userinfo",
+            headers:{authorization:req.headers.authorization}
+        }
+        await axios(options)
+        .then(res=>{
+            return next();
+        })
+        .catch(err=>{
+            res.status(401);
+            res.json({msg:"Unauthorized request"})
+
+        })
+    }
+}
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,8 @@
 let express=require("express");
 let router=express.Router();
+let auth = require("../middleware/authmiddleware");
+
+router.use(auth);
 
 router.get("/",(req,res)=>{
     res.json({msg:"Hey This Home Api"});
